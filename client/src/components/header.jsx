39c1ed69ce07715bb0758bc736844b258cec8e4e
Castle1919 <nursultan12369@gmail.com>
2024-06-17 16:07:57 +0500

    optimization 2

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,4 +1,4 @@
-import logo from "./../images/logo.png";
+import logo from "./../images/logo.webp";
 import { slide as Menu } from 'react-burger-menu';
 import React, { useState, useEffect } from "react";
 import { NavLink, useHistory } from 'react-router-dom';
